a6c52898f793854a2a2065c4008f1f2df088cea5
Ryan Peters <petersryan1993@gmail.com>
2026-03-23 13:36:41 -0400

    feat: Add Lab 6, integrate it into the Streamlit navigation, and set it as the default page.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,11 @@ lab1 = st.Page("Lab1.py", title="Lab 1", icon=":material/description:")
 lab2 = st.Page("Lab2.py", title="Lab 2", icon=":material/description:")
 lab3 = st.Page("Lab3.py", title="Lab 3", icon=":material/description:")
 lab4 = st.Page("Lab4.py", title="Lab 4", icon=":material/description:")
-lab5 = st.Page("Lab5.py", title="Lab 5", icon=":material/description:", default=True)
+lab5 = st.Page("Lab5.py", title="Lab 5", icon=":material/description:")
+lab6 = st.Page("Lab6.py", title="Lab 6", icon=":material/description:", default=True)
 
 # Create navigation
-pg = st.navigation([lab1, lab2, lab3, lab4, lab5])
+pg = st.navigation([lab1, lab2, lab3, lab4, lab5, lab6])
 
 # Configure page
 st.set_page_config(page_title="IST 488 Labs", page_icon=":material/school:")
